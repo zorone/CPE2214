28c52bf0baa2e3f762b8f996fbad99d95f415a09
Imron  Jehleh <imron.jehleh@gmail.com>
2023-08-30 16:00:29 +0700

    malloc() and free()

--- 2.3.c
+++ 2.3.c
@@ -1,14 +1,19 @@
 #include<stdio.h>
+#include<stdlib.h>
 
 int main(){
-    int num = 7;
-    int numA[5] = {3, 5, 7, 9, 11};
     int *pnum;
-    pnum = &numA[0];
-    printf("address of num = %p value = %d\n", &num, *(&num));
+    pnum = (int *) malloc(100000*sizeof(int));
 
-    for(int i = 0; i<5; i++){
-        printf("address of numA[%d] = %p value = %d\n", i, pnum+i, *(pnum+i));
+    for(int i = 0; i<100000; i++){
+        *(pnum+i) = 2*i;
     }
+
+    for(int i = 0; i<100000; i++){
+        printf("address of pnum[%d] = %p value = %d", i, pnum+i, *(pnum+i));
+    }
+
+    free(pnum);
+    
     return 0;
 }
