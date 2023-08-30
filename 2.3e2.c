803a8acc9b44e7e16434c6fdb1c1134db67d547b
Imron  Jehleh <imron.jehleh@gmail.com>
2023-08-30 15:20:02 +0700

    Add exercise files

---
+++ 2.3e2.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main(){
+    int arr[10] = {3, 5, 7, 9, 11, 543, 765643, 5341, 5667, -12134};
+    int *ptr;
+
+    ptr = &arr[0];
+
+    for(int i = 0; i<10; i++){
+        printf("num[%d] = %d\n", i, *(ptr+i));
+        ptr++;
+    }
+
+    return 0;
+}
