d3e6d3a97dfd0e8e82319223a42c98813c4d249c
Imron  Jehleh <imron.jehleh@gmail.com>
2023-09-06 14:15:02 +0700

    add new file 4-2

---
+++ 4-2_fileOpen.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+int main(int argc, const char *argv[]){
+
+    FILE *fp;
+    fp = fopen("./4-words_alpha.txt", "r");
+
+    int count = 0;
+    char tempStr[50];
+
+    while(count != EOF){
+        count = fscanf(fp, "%s", tempStr);
+        printf("%s", count);
+    }
+
+    return 0;
+}
