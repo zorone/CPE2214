7ff228d6f0e5ca1cb2dbe84dc5498622895f2ce2
Imron jehleh <imron.jehleh@gmail.com>
2023-09-13 14:47:46 +0700

    Added codes from last session. (LAB4)

---
+++ 4-3_2DimensionPtr.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+int main(int argc, const char *argv[]){
+
+    int maxNum = 400000;
+    FILE *fp;
+    char *pword[maxNum];
+    int count = 0;
+    int wcount = 0;
+    char tempStr[50];
+
+    fp = fopen("./4-words_alpha.txt", "r");
+
+    while(count != EOF){
+        count = fscanf(fp, "%s ", tempStr);
+        pword[wcount] = (char *)malloc(50);
+        strcpy(pword[wcount], tempStr);
+
+        printf("%s\n", pword[wcount]);
+        wcount++;
+
+    }
+
+    printf("wcount = %d\n", wcount);
+
+    return 0;
+}
