a08f27d70ba2e295a87d09cc2b32fb31a225e3d8
Imron jehleh <imron.jehleh@gmail.com>
2023-09-13 16:20:30 +0700

    Still doesn't work.

---
+++ 4-4_2DimensionPtr.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+int main(int argc, const char *argv[]){
+
+    int maxNum = 40000;
+    FILE *fp;
+    char *pword;
+    pword = (char*)malloc(maxNum*sizeof(char*));
+    int count = 0;
+    int wcount = 0;
+    char tempStr[50];
+
+    fp = fopen("./4-words_alpha.txt", "r");
+
+    while(count != EOF){
+        count = fscanf(fp, "%s ", tempStr);
+        pword[wcount] = (char *)malloc(strlen(tempStr));
+        strcpy(pword[wcount], tempStr);
+
+        printf("%s\n", pword[wcount]);
+        wcount++;
+
+    }
+
+    printf("wcount = %d\n", wcount);
+
+    return 0;
+}
