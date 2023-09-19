2e3250504e51446e7f313f9eb63a3d3eec2b2a41
Imron jehleh <imron.jehleh@gmail.com>
2023-09-19 23:52:50 +0700

    Change idea from using start-stop to start-limit

--- myLab/1-5_summation.c
+++ myLab/1-5_summation.c
@@ -3,14 +3,19 @@
 int summation(float start, float stop, float step);
 
 int main(){
-    float start, stop, step;
+    float start, limit, step;
     int i;
-    scanf("%f %f %f", &start, &stop, &step);
-    i = summation(start, stop, step);
+    scanf("%f %f %f", &start, &limit, &step);
+    i = summation(start, limit, step);
     printf("%d\n", i);
     return 0;
 }
 
-int summation(float start, float stop, float step){
-
+int summation(float start, float limit, float step){
+    int i, sum = start;
+    for(i = 0; sum <= limit; i++){
+        start += step;
+        sum += start;
+    }
+    return i;
 }
