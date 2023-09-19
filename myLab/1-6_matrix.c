d29f4b768ea95700d4a6d8c362362c72d9db304f
Imron jehleh <imron.jehleh@gmail.com>
2023-09-20 00:00:00 +0700

    Added matrix

---
+++ myLab/1-6_matrix.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+int generateMatrix(int row, int column);
+
+int main(){
+    int row, column, n;
+    scanf("%d %d", &row, &column);
+    n = generateMatrix(row, column);
+    printf("%d\n", n);
+    return 0;
+}
+
+int generateMatrix(int row, int column){
+    int n = 0;
+    for(int i=0; i<row; i++){
+
+        for(int j=0; j<column; j++){
+            n++;
+
+            printf("1 ");
+        }
+        printf("\n");
+    }
+}
