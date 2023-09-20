5b20dde581d006840c214235157810903f9832b1
Imron jehleh <imron.jehleh@gmail.com>
2023-09-20 15:52:08 +0700

    Initialize 6-4_linkedList.c

---
+++ LAB6-20230920/6-4_linkedList.c
@@ -0,0 +1,98 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+typedef struct listNode *plist;
+struct listNode{
+    int data;
+    plist pnext; //เก็บ Adress ของโหนด list ตัวถัดไป
+};
+
+plist createNode(void);
+plist addData(int data, plist phead);
+void travelList(plist phead);
+plist ithData(int ith, plist phead);
+void deleteIth(int ith, plist phead);
+void deleteAll(plist phead);
+
+int main(int argc, char* argv[]){
+    plist phead;
+    plist ptemp;
+    phead = createNode();
+    addData(5, phead);
+    addData(10, phead);
+    addData(29, phead);
+    addData(18, phead);
+    addData(3, phead);
+    travelList(phead);
+    ptemp = ithData(4, phead);
+    printf("From ptemp %d th position = %p data = %d\n", 4, ptemp, ptemp->data);
+
+    deleteIth(4, phead);
+    travelList(phead);
+
+    deleteAll(phead);
+    travelList(phead);
+    return 0;
+}
+
+plist createNode(void){
+    plist pnew;
+    pnew = (plist)malloc(sizeof(struct listNode));
+    pnew->data = 0;
+    pnew->pnext = NULL;
+
+    return pnew;
+}
+
+plist addData(int data, plist phead){
+    plist pnew;
+    pnew = createNode();
+    
+    if(pnew != NULL){
+    pnew->data = data;
+    pnew->pnext = phead->pnext;
+    phead->pnext = pnew;
+    }
+    return pnew;
+}
+
+void travelList(plist phead){
+    plist ptravel;
+    ptravel = phead;
+
+    while(ptravel != NULL){
+        printf("%p | %d | %p\n", ptravel, ptravel->data, ptravel->pnext);
+
+        ptravel = ptravel->pnext;
+    }
+}
+
+plist ithData(int ith, plist phead){
+    plist ptravel;
+    ptravel = phead;
+
+    for(int i=1; i<=ith; i++){
+        ptravel = ptravel->pnext;
+    }
+
+    return ptravel;
+}
+
+void deleteIth(int ith, plist phead){
+    plist pdelete, pbefore;
+    pbefore = ithData(ith-1, phead);
+    pdelete = pbefore->pnext;
+
+    pbefore->pnext = pdelete->pnext;
+    free(pdelete);
+}
+
+void deleteAll(plist phead){
+    plist ptemp, ptravel;
+    ptravel = phead;
+    while(ptravel != NULL){
+        ptemp = ptravel;
+        ptravel = ptravel->pnext;
+        free(ptemp);
+    }
+}
