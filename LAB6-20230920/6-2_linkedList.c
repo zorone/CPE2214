58cfe55322db527e734db4b5176364b78cbc3a6c
Imron jehleh <imron.jehleh@gmail.com>
2023-09-20 14:48:17 +0700

    Init 6-2_linkedList.c

---
+++ LAB6-20230920/6-2_linkedList.c
@@ -0,0 +1,69 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+typedef struct listNode *plist;
+struct listNode{
+    int data;
+    plist pnext; //เก็บ Adress ของโหนด list ตัวถัดไป
+};
+
+plist createNode(void);
+plist addData(int data, plist phead);
+void travelList(plist phead);
+plist ithData(int ith, plist phead);
+
+int main(int argc, char* argv[]){
+    plist phead;
+    phead = createNode();
+    addData(5, phead);
+    addData(10, phead);
+    addData(29, phead);
+    addData(18, phead);
+    addData(3, phead);
+    travelList(phead);
+    printf("%d th position = %p data = %d\n", 3, ithData(3, phead), ithData(3, phead)->data);
+    return 0;
+}
+
+plist createNode(void){
+    plist pnew;
+    pnew = (plist)malloc(sizeof(struct listNode));
+    pnew->data = 0;
+    pnew->pnext = NULL;
+
+    return pnew;
+}
+
+plist addData(int data, plist phead){
+    plist pnew;
+    pnew = createNode();
+    
+    if(pnew != NULL){
+    pnew->data = data;
+    pnew->pnext = phead->pnext;
+    phead->pnext = pnew;
+    }
+    return pnew;
+}
+
+void travelList(plist phead){
+    plist ptravel;
+    ptravel = phead;
+
+    while(ptravel != NULL){
+        printf("%p | %d | %p\n", ptravel, ptravel->data, ptravel->pnext);
+
+        ptravel = ptravel->pnext;
+    }
+}
+
+plist ithData(int ith, plist phead){
+    plist ptravel;
+    ptravel = phead;
+
+    for(int i=1; i<=ith; i++){
+        ptravel = ptravel->pnext;
+    }
+
+    return ptravel;
+}
