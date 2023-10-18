4800d5848a2c6edca2616c4abaec1957b70b6d3e
Imron jehleh <imron.jehleh@gmail.com>
2023-10-18 14:28:14 +0700

    Initialize class with cpp.

---
+++ LAB8-20231018/8-class.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+class stack{
+    private:
+        vector<int> arrayStack;
+        int topIndex;
+
+    public:
+        int capacity;
+        stack(int maxSize); // constructor for initial size of stack
+        void push(int data);
+        void pop(void);
+        void showStack(void);
+};
+
+stack::stack(int maxSize){
+    arrayStack.resize(maxSize);
+    topIndex = -1;
+    capacity = maxSize;
+}
+
+void stack::push(int data){
+    topIndex++;
+    arrayStack[topIndex] = data;
+}
+
+int main(void){
+    stack stackObject1(20);
+    cout << "capacity = " << stackObject1.capacity << endl;
+    // invalid: 
+    // cout << "topIndex = " << stackObject1.topIndex << endl;
+
+    return 0;
+}
