ab3dec788060b975ee8e085ce06d17c45177cd9b
Imron jehleh <imron.jehleh@gmail.com>
2023-10-18 15:26:18 +0700

    forgot to remove some codes

--- LAB8-20231018/9a-module_src.cpp
+++ LAB8-20231018/9a-module_src.cpp
@@ -27,14 +27,3 @@ void stack::showStack(){
     }
 
 }
-
-int main(void){
-    stack stackObject1(20);
-    stackObject1.push(20);
-    stackObject1.push(15);
-    stackObject1.push(10);
-    stackObject1.pop();
-    stackObject1.showStack();
-
-    return 0;
-}
