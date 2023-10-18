c3b107a39db8fc10cc3462acac473fcc911d9f0b
Imron jehleh <imron.jehleh@gmail.com>
2023-10-18 16:02:46 +0700

    try topOfStack()

--- LAB8-20231018/9c-module_main.cpp
+++ LAB8-20231018/9c-module_main.cpp
@@ -7,8 +7,8 @@ int main(void){
     stackObject1.push(10);
     stackObject1.pop();
     stackObject1.showStack();
-
+    // invalid: private data...
     // cout << stackObject1.arrayStack[stackObject1.topIndex] << endl;
-
+    cout << stackObject1.topOfStack() << endl;
     return 0;
 }
