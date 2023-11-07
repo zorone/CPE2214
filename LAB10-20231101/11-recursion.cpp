97ad68924b81b5adb3e6e89c1db0705d00524e23
Imron jehleh <imron.jehleh@gmail.com>
2023-11-07 13:42:38 +0700

    Strip some codes for more clearity.

--- LAB10-20231101/11-recursion.cpp
+++ LAB10-20231101/11-recursion.cpp
@@ -3,45 +3,29 @@
 using namespace std;
 
 int factorial(int num){   // calculate factorial of num by recursive
-
     int result = 0;
 
     if( num == 0 ){
-
         result = 1;
-
     }
-
     else{
-
         result = num*factorial(num-1);
-
     }
-
-    
 
     return result;
-
 }
 
 int summation(int num){  // calculate summation of 1 to num
-
     int result = 0;
 
     if(num == 1){
-
         result = 1;
-
     }
-
     else{
-
         result = num+summation(num-1);
-
     }
 
     return result;
-
 }
 
 int Fibonacci(int num){  // show Fibonacci
@@ -49,37 +33,24 @@ int Fibonacci(int num){  // show Fibonacci
     int result = 0;
 
     // 1,1,2,3,5,8,13,21,34,55,89
-
     if(num == 1){
-
         result = 1;
-
     }
-
     else if(num == 2){
-
         result = 1;
-
     }
-
     else if (num>2){
-
         result = Fibonacci(num-1)+Fibonacci(num-2);
-
     }
 
     return result;
-
 }
 
 int main(int argc, const char * argv[]) {
 
     cout<<"factorial = "<<factorial(5)<<endl;
-
     cout<<"summation = "<<summation(10)<<endl;
-
     cout<<"Fibonucci = "<<Fibonacci(11)<<endl;
 
     return 0;
-
 }
