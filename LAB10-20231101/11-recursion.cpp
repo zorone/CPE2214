40be39ccdd43c16ae83f8395b8fd417638b92ce7
Imron jehleh <imron.jehleh@gmail.com>
2023-11-07 18:09:26 +0700

    Change fibonacci Sequence limit.

--- LAB10-20231101/11-recursion.cpp
+++ LAB10-20231101/11-recursion.cpp
@@ -103,7 +103,7 @@ int main(int argc, const char * argv[]) {
             cout << "    " << str << "FAIL";
         }
 
-        if(i < 60){
+        if(i <= 100){
 
             if(i < 3){
                 FiboLoop[i%3] = 1;
