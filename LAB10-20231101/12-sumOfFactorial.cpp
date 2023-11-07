2ba9aa0b4598ea627892cd90e5a80af404499c9e
Imron jehleh <imron.jehleh@gmail.com>
2023-11-07 15:21:08 +0700

    ...

--- LAB10-20231101/12-sumOfFactorial.cpp
+++ LAB10-20231101/12-sumOfFactorial.cpp
@@ -1,22 +1,12 @@
 #include <iostream>
 
-int factorial(int num){
-    if(num > 1){
-        return factorial(num-1)*num;
-    }
-    else{
-        return 1;
-    }
-}
-
 int sumOfFactorial(int num){
     int result = 0;
     if(num > 1){
-        result += factorial(num);
         result += sumOfFactorial(num-1);
     }
     else{
-        result = factorial(1);
+        result = 1;
     }
 
     return result;
