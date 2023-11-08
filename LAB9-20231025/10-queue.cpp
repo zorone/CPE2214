b25780b60cafa7d6ff5272ca18a07d00ccd51df5
Imron jehleh <imron.jehleh@gmail.com>
2023-11-08 13:30:57 +0700

    forgot using namespace.

--- LAB9-20231025/10-queue.cpp
+++ LAB9-20231025/10-queue.cpp
@@ -1,6 +1,17 @@
 #include <iostream>
 #include <vector>
 
+using namespace std;
+
 class queue{
     vector<int> arrayQ;
+    int capacity;
+    int front;
+    int rear;
+
+    public:
+    queue(int capacity);
+    void enqueue(int data);
+    void dequeue(void);
+    void showQueue(void);
 };
