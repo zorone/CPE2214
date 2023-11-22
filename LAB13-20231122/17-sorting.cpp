97ae5b486cf39e01998294053909af46be55c7ee
Imron jehleh <imron.jehleh@gmail.com>
2023-11-22 15:01:06 +0700

    Implement more codes.

--- LAB13-20231122/17-sorting.cpp
+++ LAB13-20231122/17-sorting.cpp
@@ -1,7 +1,27 @@
 #include <iostream>
 #include <vector>
 
+using namespace std;
+
+void swap(vector<int> &data, int i, int j);
+void showData(vector<int> &data);
+swapping();
+
 int main(int argc, const char* argv[]){
-    vector<int> = data{3, 5, 9, 2, 7, 4, 6, 8, 1};
+    vector<int> data{3, 5, 9, 2, 7, 4, 6, 8, 1};
     showData(data);
+}
+
+void swap(vector<int> &data, int i, int j){
+    int temp;
+    temp = data[i];
+    data[i] = data[j];
+    data[j] = temp;
+}
+
+void showData(vector<int> &data){
+    for(int i = 0; i<data.size(); i++){
+        cout << data[i] << " ";
+    }
+    cout << endl;
 }
