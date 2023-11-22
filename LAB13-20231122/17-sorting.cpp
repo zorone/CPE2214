f872e21607b24a431bc3bb88efa46d10183ae7be
Imron jehleh <imron.jehleh@gmail.com>
2023-11-22 15:06:56 +0700

    Still wrong...

--- LAB13-20231122/17-sorting.cpp
+++ LAB13-20231122/17-sorting.cpp
@@ -48,7 +48,7 @@ void bubbleSort(vector<int> &data){
 
 void insertionSort(vector<int> &data){
     for(int i = 1; i < data.size(); i++){
-        for(int j = i; (data[j]<data[j-1] && (j-1 >= 0)); j--){
+        for(int j = i; (data[j]<data[j-1]) && (j-1 >= 0); j--){
             swap(data, i, i-1);
             showData(data);
         }
