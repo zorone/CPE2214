c88dd219faf6efa4b5c1eafca420138e8c3ffa50
Imron jehleh <imron.jehleh@gmail.com>
2023-12-14 21:49:10 +0700

    ...

--- myLab/8-stack.cpp
+++ myLab/8-stack.cpp
@@ -5,19 +5,19 @@
 
 using namespace std;
 
-int dataGenerator(int *data, int size);
+int dataGenerator(vector<int> *data, int size);
 
 int main(){
     vector<int> data;
     data.resize(10);
-    data = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    dataGenerator(data, 10);
     for(int i=0; i<10; i++){
         cout << data[i] << " ";
     }
     return 0;
 }
 
-int dataGenerator(int size){
+int dataGenerator(vector<int> *data, int size){
     srand(time(NULL));
     for(int i = 0; i < size; i++){
 
