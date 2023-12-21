90d8182014c73338f878551cb1a78a3c226f0e16
Imron jehleh <imron.jehleh@gmail.com>
2023-12-21 19:57:36 +0700

    Okay. That's why it's wrong.

--- others/oong_mergedSort-20231221.cpp
+++ others/oong_mergedSort-20231221.cpp
@@ -19,7 +19,7 @@ void merge(int numbers[], int temp[], int left, int mid, int right) {
     left_end = mid - 1;
     tmp_pos = left;
     num_elements = right - left + 1;
-    showStep(numbers, tmp_pos - num_elements, tmp_pos - 1);
+    showStep(numbers, left, right - 1);
 
     while ((left <= left_end) && (mid <= right)) {
         if (numbers[left] <= numbers[mid]) {
