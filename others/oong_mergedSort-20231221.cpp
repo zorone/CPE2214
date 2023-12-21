ac2cfa9f8d8e47746243d2ff06f0568df42ff32a
Imron jehleh <imron.jehleh@gmail.com>
2023-12-21 20:06:47 +0700

    That... it looks like it might work.

--- others/oong_mergedSort-20231221.cpp
+++ others/oong_mergedSort-20231221.cpp
@@ -19,8 +19,8 @@ void merge(int numbers[], int temp[], int left, int mid, int right) {
     left_end = mid - 1;
     tmp_pos = left;
     num_elements = right - left + 1;
-    tLeft = left;
-    tRight = right;
+    int tLeft = left;
+    int tRight = right;
 
     while ((left <= left_end) && (mid <= right)) {
         if (numbers[left] <= numbers[mid]) {
