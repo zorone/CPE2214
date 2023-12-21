fd387d5e445eae9dc366d19f8e49eea43adb3db2
Imron jehleh <imron.jehleh@gmail.com>
2023-12-21 19:38:19 +0700

    Ummm. I see...

--- others/oong_mergedSort-20231221.cpp
+++ others/oong_mergedSort-20231221.cpp
@@ -19,6 +19,7 @@ void merge(int numbers[], int temp[], int left, int mid, int right) {
     left_end = mid - 1;
     tmp_pos = left;
     num_elements = right - left + 1;
+    showStep(numbers, tmp_pos - num_elements, tmp_pos - 1);
 
     while ((left <= left_end) && (mid <= right)) {
         if (numbers[left] <= numbers[mid]) {
@@ -51,7 +52,7 @@ void mergeSort(int numbers[], int temp[], int left, int right) {
     int mid;
     if (right > left) {
         mid = (right + left) / 2;
-        showStep(numbers, 2*left - right + 1, left - 1);
+        showStep(numbers, left, right - 1);
         mergeSort(numbers, temp, left, mid);
         mergeSort(numbers, temp, mid + 1, right);
         merge(numbers, temp, left, mid + 1, right);
